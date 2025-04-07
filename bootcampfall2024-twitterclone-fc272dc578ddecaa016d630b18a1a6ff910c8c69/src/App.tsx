d9a32a2c62348a15c743c8c72df881032a1c0330
MJ Mlambo <mjmlambo@MJs-MacBook-Pro-589.local>
2025-04-06 23:54:54 -0400

    done

--- bootcampfall2024-twitterclone-fc272dc578ddecaa016d630b18a1a6ff910c8c69/src/App.tsx
+++ bootcampfall2024-twitterclone-fc272dc578ddecaa016d630b18a1a6ff910c8c69/src/App.tsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import Tweet from './Tweet';
+import Tweet from './tweet';
 import { tweetsData } from './data';
 
 /* Do not write your tweet component here, write it in the tweet.tsx file */
@@ -10,12 +10,16 @@ function App() {
     <div className="app">
       <h1>Twitter Clone</h1>
       <div className="feed">
-        {/* Here you will use the data from the 'tweetsData' variable and populate every tweet
-          that you received from the 'backend' (data.ts).
-         */}
+        {
+          tweetsData.map((x)=> <Tweet username={x.username} content={x.content} likes={x.likes} timestamp={x.timestamp}></Tweet>)
+        }
       </div>
     </div>
   );
 }
 
 export default App;
+
+/* Here you will use the data from the 'tweetsData' variable and populate every tweet
+          that you received from the 'backend' (data.ts).
+         */
--- bootcampfall2024-twitterclone-fc272dc578ddecaa016d630b18a1a6ff910c8c69/src/tweet.tsx
+++ bootcampfall2024-twitterclone-fc272dc578ddecaa016d630b18a1a6ff910c8c69/src/tweet.tsx
@@ -1,8 +1,8 @@
 // Tweet.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 /* 
-Create a new componnet called 'Tweet' make sure to pass down all the props you will need.
+Create a new component called 'Tweet' make sure to pass down all the props you will need.
 You will also need to use 'useState' for two variables. Then you want to create an event
 handler for when the user clicks 'like'. You can ue whatever h1s or h2s or HTML elements that 
 you want for your tweet, just make sure all the information from the screenshot I sent is there.
@@ -11,6 +11,18 @@ display a red heart "❤️" make sure to increase or decrease the like counter 
 Theres a 'tweet' class you can use to style your tweet.
 */
 
+function Tweet({username, content, likes, timestamp}) {
+  const [likeCounter, setLikes] = useState(likes);
+  const [liked, setLiked] = useState(false);
 
+  return (
+    <div className="tweet" onClick={()=>setLiked(!liked)}>
+      <h4>@{username}</h4>
+      <p>{content}</p>
+      <p>{timestamp} ago</p>
+      {liked? <button onClick={()=>setLikes(likeCounter - 1)}> ❤️ {likeCounter} Likes</button> : <button onClick={()=>setLikes(likeCounter + 1)}> 🤍 {likeCounter} Likes</button>}
+    </div>
+  );
+}
 
-// export default Tweet;
+export default Tweet;
